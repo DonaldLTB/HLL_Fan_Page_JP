a073384b13a868643c8bcaf66f1f9d0ac9a33b5d
Lawrence Kroll <larry43.lk@gmail.com>
2021-02-24 14:50:57 +0900

    routes added and authentication added in user model

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, except: [ :delete ] do
+    resources :events, only: [ :new, :create ]
+    resources :reviews, only: [ :new, :create ]
+  end
+
+  get '/users/:id/edit_schedule' =>'users#edit_schedule', as: :edit_schedule
+  patch '/users/:id/edit_schedule'=>'users#edit_schedule'
+
 end
