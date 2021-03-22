158bb7a1186302a1a81707b8436cbe819f4352f7
Lawrence Kroll <larry43.lk@gmail.com>
2021-03-22 17:12:10 +0900

    validation method change in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :given_reviews, source: :reviews, foreign_key: :reviewer_id
   has_many :received_reviews, source: :reviews, foreign_key: :user_id
   validates :username, presence: true, uniqueness: { case_sensitive: false }
-  validate :validate_username
+  # validate :validate_username
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
