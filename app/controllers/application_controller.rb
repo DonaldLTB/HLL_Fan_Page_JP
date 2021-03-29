da290a37e2e22870843034cf46516f62957076eb
Lawrence Kroll <larry43.lk@gmail.com>
2021-03-29 11:36:33 +0900

    additional authentication added when signing up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+   devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :encrypted_password, :username, :role, :bio, :rank, :discord) }
+  end
 end
