8994a67fc8319f66a096541f9bd191c2122d26eb
Lawrence Kroll <larry43.lk@gmail.com>
2021-03-30 17:05:38 +0900

    fixed index policy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
   def index
-    @users = User.all
+    # @users = User.all
+    @users = policy_scope(User)
+    # authorize @user
   end
 
   def show
     @user = User.find(params[:id])
+    authorize @user
   end
 end
