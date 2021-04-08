6161d1576e91cbb1a6781c3e6a379a1aeeff27f3
Lawrence Kroll <larry43.lk@gmail.com>
2021-04-08 21:32:00 +0900

    index added

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,25 @@
 class ReviewsController < ApplicationController
+  def index
+    @reviews = Review.all
+  end
+
   def new
     @review = Review.new
     authorize @review
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @owner = User.find(params[:user_id])
+    # @review.user_id = (params[:user_id])
+    @review.user = @owner
+    @review.reviewer = current_user
+    authorize @review
+
+    if @review.save
+      redirect_to user_path(current_user), notice: 'Review added!'
+    else
+      render :new
+    end
   end
 end
