dbf1b598858f42a94fb228fa6704494b7ba4c6ca
Lawrence Kroll <larry43.lk@gmail.com>
2021-04-08 21:28:32 +0900

    create method and new routes allowing all

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, except: [ :delete ] do
     resources :events, only: [ :new, :create ]
-    resources :reviews, only: [ :new, :create ]
+    resources :reviews
   end
 
   get '/users/:id/edit_schedule' =>'users#edit_schedule', as: :edit_schedule
