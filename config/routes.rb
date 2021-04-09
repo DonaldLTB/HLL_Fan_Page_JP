d0c091cd061bc7381d5513b9d282c88762217e06
Lawrence Kroll <larry43.lk@gmail.com>
2021-04-09 15:39:28 +0900

    get routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  
+  get '/pages/test' =>'pages#test'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, except: [ :delete ] do
     resources :events, except: [ :index ]
