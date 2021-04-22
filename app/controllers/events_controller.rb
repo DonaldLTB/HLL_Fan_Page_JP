b8231565ce74bd3b4d3d4270e96cbe7620b1762f
Lawrence Kroll <larry43.lk@gmail.com>
2021-04-22 20:27:31 +0900

    added webhooks discord

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -16,7 +16,7 @@ class EventsController < ApplicationController
     if @event.save
       # sleep(1.5)
       # raise
-      # sent_event_discord(@event.name, @event.description, @event.day, @event.start_time, @event.end_time, @team)
+      sent_event_discord(@event.name, @event.description, @event.day, @event.start_time, @event.end_time)
       redirect_to events_path
     else
       redirect_to events_path, notice: "Please try again"
@@ -31,6 +31,28 @@ class EventsController < ApplicationController
 
   def event_params
     params.require(:event).permit(:name, :description, :day, :start_time, :end_time)
+  end
+
+  WEBHOOK_URL = ENV["DISCORD"]
+  
+  def sent_event_discord(name, description, day, start_time, end_time)
+    require 'discordrb/webhooks'
+
+    client = Discordrb::Webhooks::Client.new(url: ENV["DISCORD"])
+    client.execute do |builder|
+      builder.content = "@everyone NEW Event!"
+      builder.add_embed do |embed|
+        embed.title = name
+        embed.color = 16_056_575
+        # embed.url = "http://www.clanparty.net/teams/#{team.id}/"
+        # change LINK to clanparty.net later!!!
+        embed.description = "#{description}!"
+        embed.add_field(name: 'Day', value: day, inline: true)
+        embed.add_field(name: 'Time', value: "#{start_time.strftime('%H:%M')} - #{end_time.strftime('%H:%M')}", inline: true)
+        # embed.image = Discordrb::Webhooks::EmbedImage.new(url: Event::EVENT_IMAGE[event_title])
+        embed.timestamp = Time.now
+      end
+    end
   end
 end
 
