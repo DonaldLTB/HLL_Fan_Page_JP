6aa4d5fef9974e9fd3393b84346ddbde9c595831
Lawrence Kroll <larry43.lk@gmail.com>
2021-04-23 22:57:42 +0900

    test runs

--- app/javascript/components/displaySteam.js
+++ app/javascript/components/displaySteam.js
@@ -1,27 +1,27 @@
 // const displaySteam = () => {
 
-const vmsq = require('vmsq')
+  const vmsq = require('vmsq')
 
-const stream = vmsq('hl2master.steampowered.com:27011', vmsq.EUROPE, {
+  const stream = vmsq('hl2master.steampowered.com:27011', vmsq.EUROPE, {
   gamedir: 'cstrike',
   map: 'de_nuke',
   empty: 1
-})
+  })
 
-const servers = []
+  const servers = []
 
-stream.on('error', (err) => {
+  stream.on('error', (err) => {
   console.error(err)
-})
+  })
 
-stream.on('data', (ip) => {
+  stream.on('data', (ip) => {
   console.log(ip)
   servers.push(ip)
-})
+  })
 
-stream.on('end', () => {
+  stream.on('end', () => {
   console.log(`got ${servers.length} servers`)
-})
+  })
 // }
 
 // export {displaySteam};
