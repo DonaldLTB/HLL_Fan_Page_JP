6623885899007e3c015462c673e95807a4e498fd
Lawrence Kroll <larry43.lk@gmail.com>
2021-04-23 19:44:08 +0900

    added new js file

---
+++ app/javascript/components/displaySteam.js
@@ -0,0 +1,25 @@
+const = displaySteam = () => {
+
+const vmsq = require('vmsq')
+
+const stream = vmsq('hl2master.steampowered.com:27011', vmsq.EUROPE, {
+  gamedir: 'cstrike',
+  map: 'de_nuke',
+  empty: 1
+})
+
+const servers = []
+
+stream.on('error', (err) => {
+  console.error(err)
+})
+
+stream.on('data', (ip) => {
+  console.log(ip)
+  servers.push(ip)
+})
+
+stream.on('end', () => {
+  console.log(`got ${servers.length} servers`)
+})
+}
