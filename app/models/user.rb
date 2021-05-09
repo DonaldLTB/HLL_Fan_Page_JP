03fd914ba8a0b9da485ab4b53374f4c2ce5bfd77
Lawrence Kroll <larry43.lk@gmail.com>
2021-05-09 18:08:21 +0900

    login added attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :username, presence: true, uniqueness: true
+  validates :steam_name, presence: true
+  validates :role, presence: true
   validates_format_of :username, with: /^[a-zA-Z0-9_.]*$/, multiline: true
   validate :validate_username
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get '/pages/test' =>'pages#test'
+  get '/pages/aboutus' =>'pages#aboutus'
+  get '/pages/contact' =>'pages#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, except: [ :delete ] do
     resources :events, except: [ :index ]
