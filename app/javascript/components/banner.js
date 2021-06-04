f4628949b2f8f945233150d0b89742ff1218396e
Lawrence Kroll <larry43.lk@gmail.com>
2021-06-04 12:33:07 +0900

    git slack

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -3,7 +3,7 @@ import Typed from 'typed.js';
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
     strings: ["一緒に", "Together", "Zusammen", "все вместе", "Ensemble"],
-    typeSpeed: 120,
+    typeSpeed: 121,
     loop: true
   });
 }
