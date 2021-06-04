a4d40f820c48a1a24e2bce1fa26066ebd29efdae
Lawrence Kroll <larry43.lk@gmail.com>
2021-06-04 12:30:52 +0900

    added languages to type

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["一緒に", "Together", "Zusammen"],
+    strings: ["一緒に", "Together", "Zusammen", "все вместе", "Ensemble"],
     typeSpeed: 120,
     loop: true
   });
