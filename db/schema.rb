a66878b99993e219b091536cf6de81c5a5fe72ee
Lawrence Kroll <larry43.lk@gmail.com>
2021-06-08 11:43:46 +0900

    added admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_09_111035) do
+ActiveRecord::Schema.define(version: 2021_06_08_024145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,6 +98,7 @@ ActiveRecord::Schema.define(version: 2021_05_09_111035) do
     t.time "friday_end"
     t.time "saturday_start"
     t.time "saturday_end"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
